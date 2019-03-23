31e01de607bfe14f975b740faa4ee245c124e99f
Enrique Henriquez <enrique.3427@hotmail.com>
2019-03-23 11:01:48 -0500

    Fix ortografia tutorial

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@
  */
 var baseUrl ='http://192.168.1.4';// Servidor Privada
 //var baseUrl ='http://200.116.66.129'; // Servidor Publica // sistemas.binar10.co
-//var baseUrl2 ='http://192.168.1.185';//cudris
-//var baseUrl3 ='http://192.168.1.136';//ronaldo
+//var baseUrl ='http://192.168.1.185';//cudris
+//var baseUrl ='http://192.168.1.136';//ronaldo
 export const api = {
  ipLogin : baseUrl + "/supervisores_api/public/api/login",
  ipLogout : baseUrl + "/supervisores_api/public/api/logout",
